e896311e18f532cbc28b55abbb93b55d098d8d1b
Christopher Fujino <christopherfujino@gmail.com>
2017-05-31 20:40:26 -1000

    gulp workflow now uses rollup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
 const gulp = require('gulp')
+const rollup = require('rollup')
 const babel = require('gulp-babel')
 const shell = require('gulp-shell')
 const rename = require('gulp-rename')
 
-gulp.task('babel', () => {
-  gulp.src('lib/index.es6.js')
+gulp.task('babel', ['rollup'], () => {
+  gulp.src('lib/bundle.es6.js')
     .pipe(babel({presets: ['es2015']}))
-    .pipe(rename('index.js'))
+    .pipe(rename('bundle.js'))
     .pipe(gulp.dest('lib'))
 })
 
-gulp.task('install', shell.task('npm install -g .'))
+//gulp.task('watch', () => {
+//  gulp.watch('lib/index.es6.js', ['babel', 'install'])
+//})
 
-gulp.task('watch', () => {
-  gulp.watch('lib/index.es6.js', ['babel', 'install'])
+gulp.task('rollup', () => {
+  return rollup.rollup({
+    entry: "./lib/main.es6.js"
+  }).then((bundle) => {
+    bundle.write({
+      format: "cjs",
+      dest: "./lib/bundle.es6.js"
+    })
+  })
 })
 
-gulp.task('default', ['babel', 'watch'])
+gulp.task('build', ['rollup', 'babel'])
